a53aead98c117f338c7f42380f2196d378240554
TwinkleStarrr <489385230@qq.com>
2019-11-23 16:12:16 +0800

    Revert "first commit"
    This reverts commit db74276157b4970bd6aea2df81c214172906b5aa.

--- app/src/main/java/com/example/myapplication3/MainActivity.java
+++ app/src/main/java/com/example/myapplication3/MainActivity.java
@@ -2,28 +2,13 @@ package com.example.myapplication3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1=(Button)findViewById(R.id.button_1);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("http://baidu.com"));
-                startActivity(intent);
-            }
-        });
-
     }
 }
